347d8578868d9e69715bf0242d98a76d044d4b3c
Alex <alexandercastronovo@gmail.com>
2025-02-07 23:41:31 -0500

    still black screen ._.

--- shaders/display.frag.glsl
+++ shaders/display.frag.glsl
@@ -5,5 +5,5 @@ layout(location = 0) out vec4 fragColor;
 layout(binding = 0, set = 0, rgba32f) uniform image2D hdrImage;
 
 void main() {
-    fragColor = imageLoad(hdrImage, ivec2(0, 0));
+    fragColor = imageLoad(hdrImage, ivec2(gl_FragCoord.xy));
 }
