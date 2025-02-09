c81fe870a6caa2379550c8c17139e716a9b7cc43
Alex <alexandercastronovo@gmail.com>
2025-02-08 22:51:18 -0500

    implemented tonemapping

--- shaders/display.frag.glsl
+++ shaders/display.frag.glsl
@@ -4,12 +4,19 @@ layout(location = 0) out vec4 fragColor;
 
 layout(binding = 0, set = 0, rgba32f) uniform image2D hdrImage;
 
-vec3 reinhard(vec3 hdrColor) {
+vec3 tonemapReinhard(vec3 hdrColor) {
     return hdrColor / (hdrColor + vec3(1.0));
+}
+
+vec3 tonemapACES(vec3 color) {
+    color = max(vec3(0.0), color - vec3(0.004));
+    color = (color * (6.2 * color + 0.5)) / (color * (6.2 * color + 1.7) + 0.06);
+    return color;
 }
 
 void main() {
     vec4 originalColor = imageLoad(hdrImage, ivec2(gl_FragCoord.xy));
+    vec4 tonemapped = vec4(tonemapACES(originalColor.xyz), originalColor.w);
 
-    fragColor = vec4(originalColor.xyz, originalColor.w);
+    fragColor = tonemapped;
 }
