3d8b0171034e7a6ae10ac74a14788d9a39dfa8bb
Alex <alexandercastronovo@gmail.com>
2025-02-11 21:07:15 -0500

    add buffer class

---
+++ Buffer.cpp
@@ -0,0 +1,49 @@
+
+
+#include <stdexcept>
+#include "Buffer.h"
+
+Buffer::Buffer(VkDevice logicalDevice, VkPhysicalDevice physicalDevice, VkDeviceSize dataSize, VkBufferUsageFlags usage,
+               VkMemoryAllocateFlags allocFlags, VkMemoryPropertyFlags memFlags) {
+
+    VkBufferCreateInfo createInfo{
+            .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+            .size = dataSize,
+            .usage = usage,
+            .sharingMode = VK_SHARING_MODE_EXCLUSIVE
+    };
+
+    if (vkCreateBuffer(logicalDevice, &createInfo, nullptr, &buffer) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create buffer");
+    }
+
+    VkMemoryRequirements memRequirements;
+    vkGetBufferMemoryRequirements(logicalDevice, buffer, &memRequirements);
+
+    VkMemoryAllocateFlagsInfo allocFlagsInfo{
+            .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO,
+            .flags = allocFlags
+    };
+
+    VkMemoryAllocateInfo allocInfo{
+            .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+            .pNext = &allocFlagsInfo,
+            .allocationSize = memRequirements.size,
+            .memoryTypeIndex = findMemoryType(physicalDevice, memRequirements.memoryTypeBits, memFlags)
+    };
+
+    if (vkAllocateMemory(logicalDevice, &allocInfo, nullptr, &deviceMemory) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to allocate buffer memory");
+    }
+
+    vkBindBufferMemory(logicalDevice, buffer, deviceMemory, 0);
+}
+
+VkBuffer Buffer::getBuffer() const {
+    return buffer;
+}
+
+void Buffer::destroy(VkDevice logicalDevice) {
+    vkDestroyBuffer(logicalDevice, buffer, nullptr);
+    vkFreeMemory(logicalDevice, deviceMemory, nullptr);
+}
---
+++ Buffer.h
@@ -0,0 +1,31 @@
+#ifndef RAYGUN_VK_BUFFER_H
+#define RAYGUN_VK_BUFFER_H
+
+#include <vulkan/vulkan.h>
+#include <vector>
+
+class Buffer {
+public:
+    Buffer(VkDevice logicalDevice, VkPhysicalDevice physicalDevice, VkDeviceSize dataSize, VkBufferUsageFlags usage, VkMemoryAllocateFlags allocFlags, VkMemoryPropertyFlags memFlags);
+
+    template<typename T>
+    Buffer(VkDevice logicalDevice, VkPhysicalDevice physicalDevice, const std::vector<T>& data, VkBufferUsageFlags usage, VkMemoryAllocateFlags allocFlags, VkMemoryPropertyFlags memFlags) {
+        this(logicalDevice, physicalDevice, data.empty() ? 0 : sizeof(data[0]) * data.size(), usage, allocFlags, memFlags);
+
+        void* bufferData;
+        vkMapMemory(logicalDevice, deviceMemory, 0, data.size(), 0, &bufferData);
+        memcpy(bufferData, data.data(), data.size() * sizeof(T));
+        vkUnmapMemory(logicalDevice, deviceMemory);
+    }
+
+    [[nodiscard]] VkBuffer getBuffer() const;
+
+    void destroy(VkDevice logicalDevice);
+
+private:
+    VkBuffer buffer = VK_NULL_HANDLE;
+    VkDeviceMemory deviceMemory = VK_NULL_HANDLE;
+};
+
+
+#endif //RAYGUN_VK_BUFFER_H
