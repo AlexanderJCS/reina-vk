01fe4ac427c6691c8a41a49a1d00f3ba585470b8
Alex <alexandercastronovo@gmail.com>
2025-02-14 23:00:22 -0500

    fix tonemapping kinda

--- shaders/display.frag.glsl
+++ shaders/display.frag.glsl
@@ -9,15 +9,21 @@ vec3 tonemapReinhard(vec3 hdrColor) {
 }
 
 vec3 tonemapACES(vec3 color) {
-    color = max(vec3(0.0), color - vec3(0.004));
-    color = (color * (6.2 * color + 0.5)) / (color * (6.2 * color + 1.7) + 0.06);
-    return color;
+    // https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+    // todo: implement better ACES tonemapping like at https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+
+    return clamp((color * (a * color + b)) / (color * (c * color + d) + e), vec3(0), vec3(1));
 }
 
 void main() {
     vec4 color = imageLoad(hdrImage, ivec2(gl_FragCoord.xy));
-//    vec3 tonemapped = tonemapACES(color.rgb);
+    vec3 tonemapped = tonemapACES(color.rgb);
 
     // todo: there's no sRGB conversion, but from my experience that gives poor contrast. so no sRGB for now
-    fragColor = vec4(color.xyz, color.w);
+    fragColor = vec4(tonemapped, color.w);
 }
