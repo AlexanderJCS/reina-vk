e7cd9bf7e031603487380116ec6f41d01616470e
Alex <alexandercastronovo@gmail.com>
2025-03-23 21:42:19 -0400

    fix bug with bloom not working

--- shaders/postprocessing/bloom/combine.comp.glsl
+++ shaders/postprocessing/bloom/combine.comp.glsl
@@ -1,15 +1,17 @@
 #version 460
 
+layout (local_size_x = 32, local_size_y = 8, local_size_z = 1) in;
+
 layout(binding = 0, rgba32f) readonly uniform image2D originalImage;
 layout(binding = 1, rgba32f) readonly uniform image2D bloomImage;
 layout(binding = 2, rgba32f) writeonly uniform image2D resultImage;
 
-const float BLOOM_FACTOR = 0.1;
+const float BLOOM_FACTOR = 0.025;
 
 void main() {
     ivec2 pixel = ivec2(gl_GlobalInvocationID.xy);
 
-    if (pixel.x >= imageSize(originalImage).x || pixel.y >= imageSize(originalImage).y) {
+    if (pixel.x >= imageSize(resultImage).x || pixel.y >= imageSize(resultImage).y) {
         return;
     }
 
