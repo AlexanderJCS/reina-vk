d86e8ba6b6a4d3262ea2c747541dc59d4f69e3b2
Alex <alexandercastronovo@gmail.com>
2025-03-25 21:28:15 -0400

    make blur resolution-independent

--- shaders/postprocessing/bloom/blurCommon.h.glsl
+++ shaders/postprocessing/bloom/blurCommon.h.glsl
@@ -1,7 +1,8 @@
 layout(binding = 0, rgba32f) readonly uniform image2D inImage;
 layout(binding = 1, rgba32f) writeonly uniform image2D outImage;
 
-const float RADIUS = 6.0;  // 6.0% of the screen width
+const float RADIUS = 12.0;  // 6.0% of the screen width
+const float LUMINOSITY_THRESHOLD = 2;
 const float M_PI = 3.14159265;
 
 float gauss(float x, float sigma) {
@@ -9,7 +10,7 @@ float gauss(float x, float sigma) {
     return exp(-x * x / (2.0 * sigma * sigma));
 }
 
-void blurAxis(ivec2 axis, bool threshold) {
+void blurAxis(ivec2 axis, bool applyThreshold) {
     ivec2 imageSize = imageSize(inImage);
     ivec2 pixelCoord = ivec2(gl_GlobalInvocationID.xy);
 
@@ -24,7 +25,7 @@ void blurAxis(ivec2 axis, bool threshold) {
 
     float weightSum = 0;
     for (int i = -kernelSize; i <= kernelSize; i++) {
-        float weight = gauss(float(i), 12.0);
+        float weight = gauss(float(i), RADIUS);
         weightSum += weight;
 
         ivec2 coord = pixelCoord + i * axis;
@@ -36,7 +37,7 @@ void blurAxis(ivec2 axis, bool threshold) {
         float luminosity = dot(pixel.rgb, vec3(0.299, 0.587, 0.114));
 
         // If threshold is enabled and the pixel is below threshold, skip it entirely.
-        if (threshold && luminosity < 1.0) {
+        if (applyThreshold && luminosity < LUMINOSITY_THRESHOLD) {
             continue;
         }
 
