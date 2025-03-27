ede2a468a1b4d4f0f89e4d74befb3b96e3a0c7ef
Alex <alexandercastronovo@gmail.com>
2025-03-27 18:54:59 -0400

    enable shader optimizations

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,15 +1,15 @@
-glslc -fshader-stage=rgen --target-env=vulkan1.3 ./raytrace/raytrace.rgen.glsl -o ./raytrace/raytrace.rgen.spv
-glslc -fshader-stage=rmiss --target-env=vulkan1.3 ./raytrace/raytrace.rmiss.glsl -o ./raytrace/raytrace.rmiss.spv
-glslc -fshader-stage=rchit --target-env=vulkan1.3 ./raytrace/lambertian.rchit.glsl -o ./raytrace/lambertian.rchit.spv
-glslc -fshader-stage=rchit --target-env=vulkan1.3 ./raytrace/metal.rchit.glsl -o ./raytrace/metal.rchit.spv
-glslc -fshader-stage=rchit --target-env=vulkan1.3 ./raytrace/dielectric.rchit.glsl -o ./raytrace/dielectric.rchit.spv
-glslc -fshader-stage=rmiss --target-env=vulkan1.3 ./raytrace/shadow.rmiss.glsl -o ./raytrace/shadow.rmiss.spv
+glslc -O -fshader-stage=rgen --target-env=vulkan1.3 ./raytrace/raytrace.rgen.glsl -o ./raytrace/raytrace.rgen.spv
+glslc -O -fshader-stage=rmiss --target-env=vulkan1.3 ./raytrace/raytrace.rmiss.glsl -o ./raytrace/raytrace.rmiss.spv
+glslc -O -fshader-stage=rchit --target-env=vulkan1.3 ./raytrace/lambertian.rchit.glsl -o ./raytrace/lambertian.rchit.spv
+glslc -O -fshader-stage=rchit --target-env=vulkan1.3 ./raytrace/metal.rchit.glsl -o ./raytrace/metal.rchit.spv
+glslc -O -fshader-stage=rchit --target-env=vulkan1.3 ./raytrace/dielectric.rchit.glsl -o ./raytrace/dielectric.rchit.spv
+glslc -O -fshader-stage=rmiss --target-env=vulkan1.3 ./raytrace/shadow.rmiss.glsl -o ./raytrace/shadow.rmiss.spv
 
-glslc -fshader-stage=vert --target-env=vulkan1.3 ./raster/display.vert.glsl -o ./raster/display.vert.spv
-glslc -fshader-stage=frag --target-env=vulkan1.3 ./raster/display.frag.glsl -o ./raster/display.frag.spv
+glslc -O -fshader-stage=vert --target-env=vulkan1.3 ./raster/display.vert.glsl -o ./raster/display.vert.spv
+glslc -O -fshader-stage=frag --target-env=vulkan1.3 ./raster/display.frag.glsl -o ./raster/display.frag.spv
 
-glslc -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/tonemap/postprocessing.comp.glsl -o ./postprocessing/tonemap/postprocessing.comp.spv
+glslc -O -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/tonemap/postprocessing.comp.glsl -o ./postprocessing/tonemap/postprocessing.comp.spv
 
-glslc -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/bloom/blurX.comp.glsl -o ./postprocessing/bloom/blurX.comp.spv
-glslc -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/bloom/blurY.comp.glsl -o ./postprocessing/bloom/blurY.comp.spv
-glslc -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/bloom/combine.comp.glsl -o ./postprocessing/bloom/combine.comp.spv
+glslc -O -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/bloom/blurX.comp.glsl -o ./postprocessing/bloom/blurX.comp.spv
+glslc -O -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/bloom/blurY.comp.glsl -o ./postprocessing/bloom/blurY.comp.spv
+glslc -O -fshader-stage=comp --target-env=vulkan1.3 ./postprocessing/bloom/combine.comp.glsl -o ./postprocessing/bloom/combine.comp.spv
